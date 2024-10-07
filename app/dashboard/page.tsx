9228b089ead637ea3b54132275468ff7ddf8eda3
jaysenjonsin <jasoncjohnson5@yahoo.com>
2024-10-06 21:18:44 -0700

    set up dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from 'next';
+import React from 'react';
+import Main from '../../components/Main';
+import Dashboard from '../../components/Dashboard';
+import Login from '../../components/Login';
+
+export const metadata: Metadata = {
+  title: 'moodmoji dashboard',
+};
+type Props = {};
+
+const DashboardPage = (props: Props) => {
+  let isAuthenticated = false;
+  let children = <Login />;
+
+  if (isAuthenticated) {
+    children = <Dashboard />;
+  }
+  return <Main>{children}</Main>;
+};
+
+export default DashboardPage;
---
+++ components/Main.tsx
@@ -0,0 +1,8 @@
+import React, { ReactNode } from 'react';
+
+type Props = { children: ReactNode };
+const Main = ({ children }: Props) => {
+  return <main>{children}</main>;
+};
+
+export default Main;
