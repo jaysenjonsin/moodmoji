4e0c526dbd07f6fdce44ac1c0eaab9f15e62014d
jaysenjonsin <jasoncjohnson5@yahoo.com>
2024-10-06 22:03:48 -0700

    add font

--- components/Main.tsx
+++ components/Main.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react';
 
 type Props = { children: ReactNode };
 const Main = ({ children }: Props) => {
-  return <main>{children}</main>;
+  return <main className='flex-1'>{children}</main>;
 };
 
 export default Main;
