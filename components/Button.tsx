5276009dca61dcfe9c391a14ff8ac47e1571c4f7
jaysenjonsin <jasoncjohnson5@yahoo.com>
2024-10-08 22:48:15 -0700

    hero and button styling

---
+++ components/Button.tsx
@@ -0,0 +1,26 @@
+import { Fugaz_One } from 'next/font/google';
+import React from 'react';
+const fugaz = Fugaz_One({ subsets: ['latin'], weight: ['400'] });
+
+type Props = { text: string; dark?: boolean };
+
+const Button = ({ text, dark }: Props) => {
+  return (
+    <button
+      className={
+        `rounded-full overflow-hidden border-2 duration-200 hover:opacity-60 border-solid border-indigo-600 ` +
+        (dark ? 'text-white bg-indigo-600 ' : 'text-indigo-600 ')
+      }
+    >
+      <p
+        className={
+          `px-7 sm:px-10 whitespace-nowrap py-2 sm:py-3 ` + fugaz.className
+        }
+      >
+        {text}
+      </p>
+    </button>
+  );
+};
+
+export default Button;
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
-
+import { Fugaz_One } from 'next/font/google';
+import Button from './Button';
+const fugaz = Fugaz_One({ subsets: ['latin'], weight: ['400'] });
 type Props = {};
 
 const Hero = (props: Props) => {
   return (
-    <div className='py-10 sm:py-14 md:py-20'>
-      <h1>
-        <span>moodmoji</span> tracks your <span>daily</span>mood
+    <div className='py-4 md:py-10 flex flex-col gap-4 sm:gap-8'>
+      <h1
+        className={
+          `text-5xl sm:text-6xl md:text-7xl text-center ` + fugaz.className
+        }
+      >
+        <span className='textGradient'>moodmoji</span> tracks your
+        <span className='textGradient'> daily</span> mood
       </h1>
+      <p className='text-lg sm:text-xl md:text-2xl text-center w-full mx-auto max-w[600px]'>
+        Create your mood record and see how you feel on{' '}
+        <span className='font-semibold'>Every day of every year</span>
+      </p>
+      <div className='grid grid-cols-2 gap-4'>
+        <Button text='Sign Up' />
+        <Button text='Login' dark />
+      </div>
     </div>
   );
 };
