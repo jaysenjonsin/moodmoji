d42ebba9b0e58567a1492a5785559f30b201b532
jaysenjonsin <jasoncjohnson5@yahoo.com>
2024-10-22 20:23:29 -0700

    start calendar

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,8 +1,43 @@
+import { Fugaz_One } from 'next/font/google';
 import React from 'react';
 
 type Props = {};
 
+const months = {
+  January: 'Jan',
+  February: 'Feb',
+  March: 'Mar',
+  April: 'Apr',
+  May: 'May',
+  June: 'Jun',
+  July: 'Jul',
+  August: 'Aug',
+  September: 'Sept',
+  October: 'Oct',
+  November: 'Nov',
+  December: 'Dec',
+};
+const now = new Date();
+const dayList = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+];
+
+const fugaz = Fugaz_One({ subsets: ['latin'], weight: ['400'] });
 const Calendar = (props: Props) => {
+  const year = 2024;
+  const month = 'July';
+  const monthNow = new Date(year, Object.keys(months).indexOf(month), 1); //first day of curr month
+  const firstDayOfMonth = monthNow.getDay(); //which day was first day
+  const daysInMonth = new Date(year, Object.keys(month).indexOf(month) + 1, 0); //day before first of month
+
+  const daysToDisplay = firstDayOfMonth + daysInMonth;
+  const numRows = Math.floor(daysToDisplay / 7) + (daysToDisplay % 7 ? 1 : 0);
   return <div>Calendar</div>;
 };
 
--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Fugaz_One } from 'next/font/google';
+import Calendar from './Calendar';
 type Props = {};
 const fugaz = Fugaz_One({ subsets: ['latin'], weight: ['400'] });
 
@@ -17,17 +18,16 @@ const Dashboard = (props: Props) => {
     good: '😊',
     elated: '😍',
   };
-  console.log('hi');
   return (
-    <div className='flex flex-col flex-1 gap-4 sm:gap-8 md:gap-12'>
-      <div className='grid grid-cols-1 sm:grid-cols-3 bg-indigo-50 text-indigo-500 rounded-lg'>
+    <div className='flex flex-col flex-1 gap-8 sm:gap-12 md:gap-16'>
+      <div className='grid grid-cols-3 p-4 gap-4 bg-indigo-50 text-indigo-500 rounded-lg'>
         {Object.keys(statuses).map((status, idx) => {
           return (
-            <div key={idx} className='p-4 flex flex-col gap-1 sm:gap-2'>
-              <p className='font-medium uppercase text-xs sm:text-sm'>
+            <div key={idx} className='flex flex-col gap-1 sm:gap-2'>
+              <p className='font-medium uppercase text-xs sm:text-sm truncate'>
                 {status.replaceAll('_', ' ')}
               </p>
-              <p className={`text-base sm:text-lg ` + fugaz.className}>
+              <p className={`text-base sm:text-lg truncate ` + fugaz.className}>
                 {statuses[status as keyof typeof statuses]}
               </p>
             </div>
@@ -41,15 +41,28 @@ const Dashboard = (props: Props) => {
       >
         How do you <span className='textGradient'>Feel</span> today?
       </h4>
-      <div className='grid grid-cols-2 md:grid-cols-5 gap-4'>
+      <div className='flex items-stretch flex-wrap gap-4'>
         {Object.keys(moods).map((mood, i) => (
-          <button className='' key={i}>
-            <p>{mood}</p>
+          <button
+            className={`p-4 px-5 rounded-2xl purpleShadow duration-200 bg-indigo-50 hover:bg-indigo-200 text-center flex flex-col items-center gap-2 flex-1 `}
+            key={i}
+          >
             {/* ts doesnt know values in object.keys(moods) will always be valid keys for moods object, so tell ts mood variable will always be one of keys of moods obj */}
-            <p>{moods[mood as keyof typeof moods]}</p>
+            <p className='text-4xl sm:text-5xl md:text-6xl'>
+              {moods[mood as keyof typeof moods]}
+            </p>
+            <p
+              className={
+                `text-indigo-500 text-xs sm:text-sm md:text-base ` +
+                fugaz.className
+              }
+            >
+              {mood}
+            </p>
           </button>
         ))}
       </div>
+      <Calendar />
     </div>
   );
 };
