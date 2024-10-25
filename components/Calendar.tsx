339749729ddf8b1b0a4c6efb787d1247ae17593e
jaysenjonsin <jasoncjohnson5@yahoo.com>
2024-10-24 19:08:02 -0700

    calender display logic

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -34,11 +34,32 @@ const Calendar = (props: Props) => {
   const month = 'July';
   const monthNow = new Date(year, Object.keys(months).indexOf(month), 1); //first day of curr month
   const firstDayOfMonth = monthNow.getDay(); //which day was first day
-  const daysInMonth = new Date(year, Object.keys(month).indexOf(month) + 1, 0); //day before first of month
+  const daysInMonth = new Date(
+    year,
+    Object.keys(month).indexOf(month) + 1,
+    0
+  ).getDate(); //day before first of month
 
   const daysToDisplay = firstDayOfMonth + daysInMonth;
-  const numRows = Math.floor(daysToDisplay / 7) + (daysToDisplay % 7 ? 1 : 0);
-  return <div>Calendar</div>;
+  const numRows = Math.floor(daysToDisplay / 7) + (daysToDisplay % 7 ? 1 : 0); //if start on ex. thurs, add on how many rows we need, if any leftovers add extra row
+  return (
+    <div className='flex flex-col overflow-hidden gap-1'>
+      {[...Array(numRows).keys()].map((row, rowIndex) => (
+        <div key={rowIndex}>
+          {/* cant use implicit return here bc inner calculations */}
+          {dayList.map((dayOfWeek, dayOfWeekIndex) => {
+            //calculate curr day
+            let dayIndex =
+              rowIndex * 7 + dayOfWeekIndex - (firstDayOfMonth - 1);
+              //--> this determines blanks ex. if first of month wed, mon tues blank 
+              let dayDisplay = dayIndex > daysInMonth ? false : (row === 0 && dayOfWeekIndex < firstDayOfMonth) ? false : true
+
+            return <div key = {dayOfWeek}>{dayOfWeekIndex} </div>;
+          })}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Calendar;
